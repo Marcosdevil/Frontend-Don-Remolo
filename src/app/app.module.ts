b0f2d181327b33511585d5f9cfa2be1f8107f05d
usuario <przmarcos@gmail.com>
2022-03-07 13:44:04 -0300

    Frontend Don Remolo con Angular 13. Creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { OrdersComponent } from './orders/orders.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    OrdersComponent,
+    CategoriesComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule
